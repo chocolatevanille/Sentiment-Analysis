cb7bbeccaec373e0553e80d98b248facca5bd0d9
willster191 <William.C.B.19@gmail.com>
2024-07-13 12:32:38 -0500

    Project completed
    Initial completion of sentiment analysis project
    Plan to experiment with different data for model training

--- main.py
+++ main.py
@@ -20,15 +20,35 @@ gui.theme("DarkBlue")
 # it's me!
 def display_credits():
     layout = [
-        [gui.Text("Made by Noël Barron",justification='center')],
+        [gui.Text("Made by Noël Barron",justification='center',font=("Open Sans"))],
+        [gui.Text("AI model trained on Amazon Review Data (2018)",justification='left',font=("Open Sans"))],
+        [gui.Text("Ni, J., Li, J., & McAuley, J. (n.d.). Justifying recommendations using distantly-labeled reviews and fined-grained aspects. Empirical Methods in Natural Language Processing (EMNLP), 2019",justification='left',font=("Open Sans"))],
         [gui.Column([[gui.Button('GitHub',enable_events=True,key='-GITHUB-')]],
         justification='center')]
     ]
     window = gui.Window("Credits", layout, modal=True)
-    url = 'https://github.com/chocolatevanille/PantheonofHallownestTracker'
+    url = 'https://github.com/chocolatevanille/Sentiment-Analysis'
     while True:
         event, values = window.read()
         if event == '-GITHUB-':
+            webbrowser.open(url)
+        elif event == gui.WIN_CLOSED:
+            window.close()
+            return None
+        
+def display_about():
+    layout = [
+        [gui.Text("AI Information",justification='center',font=("Open Sans", 14))],
+        [gui.Text("This AI was trained on Amazon movie and TV reviews",justification='left')],
+        [gui.Text("The results are not guaranteed to be accurate",justification='left')],
+        [gui.Column([[gui.Button('Training Data',enable_events=True,key='-DATA-')]],
+        justification='center')]
+    ]
+    window = gui.Window("About", layout, modal=True)
+    url = 'https://nijianmo.github.io/amazon/index.html'
+    while True:
+        event, values = window.read()
+        if event == '-DATA-':
             webbrowser.open(url)
         elif event == gui.WIN_CLOSED:
             window.close()
@@ -62,14 +82,14 @@ def get_sentiment(link):
     base_url = "https://www.googleapis.com/youtube/v3/commentThreads"
 
     while True:
-        # Construct API request URL
+        # construct API request URL
         comments_url = f"{base_url}?part=snippet&videoId={video_id}&key={api_key}&maxResults={max_results}&pageToken={next_page_token if next_page_token is not None else ''}"
 
-        # Make API request
+        # make API request
         comments_response = requests.get(comments_url)
         comments_data = comments_response.json()
 
-        # Extract comments from the response
+        # extract comments from the response
         for item in comments_data["items"]:
             comment_text = item["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
             comment_text = str(comment_text)
@@ -78,16 +98,16 @@ def get_sentiment(link):
             comment_text = comment_text.lower()
             comments.append(comment_text)
 
-        # Check if there are more pages to fetch
+        # check if there are more pages to fetch
         if 'nextPageToken' in comments_data:
             next_page_token = comments_data['nextPageToken']
         else:
-            break  # No more pages, exit loop
+            break  # no more pages, exit loop
         pages += 1
-        if pages >= 30:
+        if pages >= 5: # obtain max 500 comments for now
             break
 
-    print(f"Number of comments: {len(comments)}")
+    #print(f"Number of comments: {len(comments)}")
 
 
     # load model and tokenizer
@@ -102,7 +122,6 @@ def get_sentiment(link):
     # inputs = tokenizer(comment, return_tensors='pt')
 
     # tokenize all comments
-    # Tokenize input texts
     inputs = tokenizer.batch_encode_plus(
        comments,
        return_tensors='pt',  # return PyTorch tensors
@@ -110,13 +129,13 @@ def get_sentiment(link):
        truncation=True       # truncate comments longer than the maximum length
     )
 
-    # Forward pass and prediction
+    # forward pass and prediction
     with torch.no_grad():
         outputs = model(**inputs)
     logits = outputs.logits
     predicted_classes = logits.argmax(dim=1).tolist()
 
-    # Map predicted class to sentiment label
+    # map predicted class to sentiment label
     sentiment_labels = ["Very Negative", "Negative", "Neutral", "Positive", "Very Positive"]
     predicted_sentiments = [sentiment_labels[pred] for pred in predicted_classes]
 
@@ -125,17 +144,12 @@ def get_sentiment(link):
         ind = sentiment_labels.index(sentiment)
         sentiment_tally[ind] = sentiment_tally[ind] + 1
 
-    # get total number of sentiments
-    # there is a faster way to do this (number of reviews), implement later
-    sentiment_count = 0
-    for i in sentiment_tally:
-        sentiment_count += i
-    print(f"Total number of reviews: {sentiment_count}")
+    sentiment_count = len(comments)
     
     sentiment_percentage = []
     for i in sentiment_tally:
         sentiment_percentage.append(i*100/sentiment_count)
-    print(f"Sentiment Percentages: {sentiment_percentage}")
+    #print(f"Sentiment Percentages: {sentiment_percentage}")
     
     bar_layout = [
         [gui.Graph(canvas_size=(500, 400), graph_bottom_left=(0, 0), graph_top_right=(500, 400), key='-GRAPH-')],
@@ -145,15 +159,10 @@ def get_sentiment(link):
     graph = sentiments_window['-GRAPH-']
 
     for i, value in enumerate(sentiment_percentage):
-        graph.draw_rectangle(top_left=(i*75 + 75, value * 3.5 + 50),bottom_right=(i*75 + 125, 50),fill_color='Purple')
+        graph.draw_rectangle(top_left=(i*75 + 75, value * 3 + 50),bottom_right=(i*75 + 125, 50),fill_color='Purple')
+        graph.draw_text(sentiment_labels[i], (i*75 + 100, 30), color='Pink')
+        graph.draw_text(str(round(sentiment_percentage[i],2))+'%', (i*75 + 102, 13), color='Pink')
 
-    #bar_width = 50
-    #padding = 20
-    #for i, (sentiment, value) in enumerate(zip(sentiment_labels, sentiment_tally)):
-    #    graph.draw_rectangle(top_left=(i * (bar_width + padding) + padding, 300 - value * 5),
-    #                        bottom_right=((i + 1) * bar_width + i * padding, 300),
-    #                        fill_color='blue')
-    #    graph.draw_text(sentiment, (i * (bar_width + padding) + padding + 10, 280), color='black')
     while True:
         event, values = sentiments_window.read()
         if event == gui.WINDOW_CLOSED or event == '-CLOSE-SENTIMENTS-':
@@ -161,13 +170,14 @@ def get_sentiment(link):
     sentiments_window.close()
 
 # designing the main window layout
-layout_top = [  [gui.Text("YouTube Sentiment Analysis")]]
+layout_top = [  [gui.Text("YouTube Sentiment Analysis",font=("Open Sans",14))]]
             
 layout_middle = [gui.Text("Link:"),
                  gui.Input(default_text="https://www.youtube.com/watch?v=YbJOTdZBX1g",size=(100,12),tooltip="Your link goes here.",border_width=5,focus=True,key='-LINK-')
 ]
 
 layout_bottom = [gui.Column([[gui.Button('Go!',key='-GO-',button_color="Green")]]),
+                 gui.Column([[gui.Button('About',key='-ABOUT-')]]),
                  gui.Column([[gui.Button('Credits',key='-CREDITS-')]]),
                  gui.Column([[gui.Button('Close',key='-CLOSE-')]])
 ]
@@ -181,7 +191,7 @@ layout = [
 ]
 
 # main window creation
-window = gui.Window('YT Sentiment Analysis', layout,size=(750,540),element_justification='center',resizable=True,return_keyboard_events=True)
+window = gui.Window('YT Sentiment Analysis', layout,size=(750,130),element_justification='center',resizable=True,return_keyboard_events=True)
 
 # event loop
 def win_run():
@@ -192,6 +202,9 @@ def win_run():
         elif event == '-GO-' or event == '\r':
             link = values['-LINK-']
             get_sentiment(link)
+        elif event == '-ABOUT-':
+            display_about()
+            continue
         elif event == '-CREDITS-': # when the user wants to see who made the program
             display_credits() # hey it's me! wait didn't i already do that jo-
             continue
